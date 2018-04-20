c265ab8b9780b0ab8d0418756e4cdd2be4fb037b
6谢侠6 <xiechong23@icloud.com>
2018-04-21 00:17:35 +0800

    Initial Commit

---
+++ XDXRatingBar/XDXRatingBar.h
@@ -0,0 +1,19 @@
+//
+//  XDXRatingBar.h
+//  XDXRatingBar
+//
+//  Created by Chong Xie on 2018/4/21.
+//  Copyright © 2018年 6谢侠6 (6xieapplexia6). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XDXRatingBar.
+FOUNDATION_EXPORT double XDXRatingBarVersionNumber;
+
+//! Project version string for XDXRatingBar.
+FOUNDATION_EXPORT const unsigned char XDXRatingBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XDXRatingBar/PublicHeader.h>
+
+
